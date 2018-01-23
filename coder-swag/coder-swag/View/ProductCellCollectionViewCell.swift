8206fcb446ed7e4da4df63fb5256f0869b631e4e
George Buckley <g.buckley@live.com>
2018-01-23 13:23:39 +0000

    Added product collection view in storyboard

---
+++ coder-swag/coder-swag/View/ProductCellCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by George Buckley on 23/01/2018.
+//  Copyright © 2018 George Buckley. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
