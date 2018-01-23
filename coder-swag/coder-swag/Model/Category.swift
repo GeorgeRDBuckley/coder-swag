a792c515e1c09ec47e10c293d9349325ce946832
George Buckley <g.buckley@live.com>
2018-01-23 13:03:38 +0000

    Dummy data, reusable cells

---
+++ coder-swag/coder-swag/Model/Category.swift
@@ -0,0 +1,19 @@
+//
+//  Category.swift
+//  coder-swag
+//
+//  Created by George Buckley on 23/01/2018.
+//  Copyright © 2018 George Buckley. All rights reserved.
+//
+
+import Foundation
+
+struct Category {
+    private(set) public var title: String
+    private(set) public var imageName: String
+    
+    init(title: String, imageName: String) {
+        self.title = title
+        self.imageName = imageName
+    }
+}
---
+++ coder-swag/coder-swag/Services/DataService.swift
@@ -0,0 +1,24 @@
+//
+//  DataService.swift
+//  coder-swag
+//
+//  Created by George Buckley on 23/01/2018.
+//  Copyright © 2018 George Buckley. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    static let instance = DataService()
+    
+    private let categories = [
+        Category(title: "SHIRTS", imageName: "shirts.png"),
+        Category(title: "HOODIES", imageName: "hoodies.png"),
+        Category(title: "HATS", imageName: "hats.png"),
+        Category(title: "DIGITAL", imageName: "digital.png")
+    ]
+    
+    func getCategories() -> [Category] {
+        return categories
+    }
+}
