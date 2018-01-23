acdf03166bcc2579c3e80ddc4a94c4e6ffa38cfe
George Buckley <g.buckley@live.com>
2018-01-23 14:09:54 +0000

    Setup data for products

---
+++ coder-swag/coder-swag/Model/Product.swift
@@ -0,0 +1,21 @@
+//
+//  Product.swift
+//  coder-swag
+//
+//  Created by George Buckley on 23/01/2018.
+//  Copyright © 2018 George Buckley. All rights reserved.
+//
+
+import Foundation
+
+struct Product {
+    private(set) public var title: String
+    private(set) public var price: String
+    private(set) public var imageName: String
+    
+    init(title: String, price: String, imageName: String) {
+        self.title = title
+        self.price = price
+        self.imageName = imageName
+    }
+}
